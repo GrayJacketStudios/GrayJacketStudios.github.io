4e174d2e7ae784611b1bf35ebe4dd2c19adcf25b
Sebastian <grayjacketstudios@gmail.com>
2018-12-12 18:49:47 -0300

    Se añade la funcionalidad de menu responsivo

--- src/components/cabecera/Menu.js
+++ src/components/cabecera/Menu.js
@@ -4,11 +4,22 @@ import { Link } from 'react-router-dom';
 import '../../css/menu.css';
 
 export default class Menu extends Component {
+
+    myFunction() {
+        var x = document.getElementById("menu");
+        if (x.className === "menu") {
+            x.className += " responsive";
+        } else {
+            x.className = "menu";
+        }
+    }
+
+
     render() {
         return (
-            <div className="menu">
+            <div >
                 <nav>
-                    <ul>
+                    <ul className="menu" id="menu">
                         <li><Link to="/">INICIO</Link></li>
                         <li><Link to="/">QUE HACEMOS</Link></li>
                         <li><Link to="/">TECNOLOGIAS</Link></li>
@@ -16,6 +27,9 @@ export default class Menu extends Component {
                         <li><Link to="/">NUESTROS JUEGOS</Link></li>
                         <li><Link to="/">QUIENES SOMOS</Link></li>
                         <li><Link to="/">CONTACTANOS</Link></li>
+                        <li><a href="javascript:void(0);" class="icon" onClick={this.myFunction}>
+                            <i class="fa fa-bars"></i>
+                        </a></li>
                     </ul>
 
                 </nav>
