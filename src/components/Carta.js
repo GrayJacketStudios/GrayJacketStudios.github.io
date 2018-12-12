50d7088f01241965ee32a6acb78c34d115163d6e
Sebastian <grayjacketstudios@gmail.com>
2018-12-12 14:37:20 -0300

    pequeña incorporacion

--- src/components/Carta.js
+++ src/components/Carta.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react'
 export default class Carta extends Component {
     render() {
         return (
-            <div>
+            <div className="Carta">
                 {this.props.children}
             </div>
         )
