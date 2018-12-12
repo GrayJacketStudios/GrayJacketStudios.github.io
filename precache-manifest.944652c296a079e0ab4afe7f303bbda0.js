1fa9f3fbaef9dd277046c30b26ca36894d6a0b79
Sebastian <grayjacketstudios@gmail.com>
2018-12-12 10:26:16 -0300

    Updates

---
+++ precache-manifest.944652c296a079e0ab4afe7f303bbda0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "2264a741a0670fbfc586",
+    "url": "/static/js/main.2264a741.chunk.js"
+  },
+  {
+    "revision": "82dafdb578ba27673946",
+    "url": "/static/js/1.82dafdb5.chunk.js"
+  },
+  {
+    "revision": "2264a741a0670fbfc586",
+    "url": "/static/css/main.f958a69e.chunk.css"
+  },
+  {
+    "revision": "7fab76610d4db274396c69f9a06398df",
+    "url": "/index.html"
+  }
+];
